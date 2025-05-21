c44bf790c802cbfb434d269db651b371a0a68de1
AndresinhoXDD <154407569+AndresinhoXDD@users.noreply.github.com>
2025-05-20 23:57:10 -0500

    commit5

--- recursos/Basedatos/biblioteca.sql
+++ recursos/Basedatos/biblioteca.sql
@@ -1,202 +1,197 @@
--- MySQL Workbench Forward Engineering
+CREATE DATABASE  IF NOT EXISTS `biblioteca` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `biblioteca`;
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: biblioteca
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema biblioteca
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS biblioteca ;
+--
+-- Table structure for table `autor`
+--
 
--- -----------------------------------------------------
--- Schema biblioteca
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS biblioteca DEFAULT CHARACTER SET utf8mb4 ;
-USE biblioteca ;
+DROP TABLE IF EXISTS `autor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `autor` (
+  `autor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `autor_nombre` varchar(100) NOT NULL,
+  `autor_nacionalidad` varchar(50) DEFAULT NULL,
+  `autor_fecha_nacimiento` date DEFAULT NULL,
+  PRIMARY KEY (`autor_id`),
+  UNIQUE KEY `nombre` (`autor_nombre`),
+  KEY `idx_autor_nombre` (`autor_nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table biblioteca.autor
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.autor ;
+--
+-- Table structure for table `ejemplar`
+--
 
-CREATE TABLE IF NOT EXISTS biblioteca.autor (
-  autor_id INT(11) NOT NULL AUTO_INCREMENT,
-  autor_nombre VARCHAR(100) NOT NULL,
-  autor_nacionalidad VARCHAR(50) NULL DEFAULT NULL,
-  autor_fecha_nacimiento DATE NULL DEFAULT NULL,
-  PRIMARY KEY (autor_id),
-  UNIQUE INDEX nombre (autor_nombre ASC) ,
-  INDEX idx_autor_nombre (autor_nombre ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+DROP TABLE IF EXISTS `ejemplar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ejemplar` (
+  `ejemplar_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ejemplar_libro_id` int(11) NOT NULL,
+  `ejemplar_estado` enum('disponible','prestado','en_mora','perdido') DEFAULT 'disponible',
+  PRIMARY KEY (`ejemplar_id`),
+  KEY `libro_id` (`ejemplar_libro_id`),
+  KEY `idx_ejemplar_estado` (`ejemplar_estado`),
+  CONSTRAINT `ejemplar_ibfk_1` FOREIGN KEY (`ejemplar_libro_id`) REFERENCES `libro` (`libro_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `libro`
+--
 
--- -----------------------------------------------------
--- Table biblioteca.libro
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.libro ;
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libro` (
+  `libro_id` int(11) NOT NULL AUTO_INCREMENT,
+  `libro_titulo` varchar(255) NOT NULL,
+  `libro_isbn` varchar(13) NOT NULL,
+  `libro_copias_totales` int(11) DEFAULT 0,
+  `libro_copias_disponibles` int(11) DEFAULT 0,
+  PRIMARY KEY (`libro_id`),
+  UNIQUE KEY `isbn` (`libro_isbn`),
+  KEY `idx_libro_titulo` (`libro_titulo`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS biblioteca.libro (
-  libro_id INT(11) NOT NULL AUTO_INCREMENT,
-  libro_titulo VARCHAR(255) NOT NULL,
-  libro_isbn VARCHAR(13) NOT NULL,
-  libro_copias_totales INT(11) NULL DEFAULT 0,
-  libro_copias_disponibles INT(11) NULL DEFAULT 0,
-  PRIMARY KEY (libro_id),
-  UNIQUE INDEX isbn (libro_isbn ASC) ,
-  INDEX idx_libro_titulo (libro_titulo ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+--
+-- Table structure for table `libroautor_libroautor`
+--
 
+DROP TABLE IF EXISTS `libroautor_libroautor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libroautor_libroautor` (
+  `libroautor_id` int(11) NOT NULL AUTO_INCREMENT,
+  `libroautor_libro_id` int(11) NOT NULL,
+  `autor_id` int(11) NOT NULL,
+  PRIMARY KEY (`libroautor_id`),
+  KEY `autor_id` (`autor_id`),
+  KEY `libroautor_ibfk_1` (`libroautor_libro_id`),
+  CONSTRAINT `libroautor_ibfk_1` FOREIGN KEY (`libroautor_libro_id`) REFERENCES `libro` (`libro_id`) ON DELETE CASCADE,
+  CONSTRAINT `libroautor_ibfk_2` FOREIGN KEY (`autor_id`) REFERENCES `autor` (`autor_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table biblioteca.ejemplar
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.ejemplar ;
+--
+-- Table structure for table `prestamo`
+--
 
-CREATE TABLE IF NOT EXISTS biblioteca.ejemplar (
-  ejemplar_id INT(11) NOT NULL AUTO_INCREMENT,
-  ejemplar_libro_id INT(11) NOT NULL,
-  ejemplar_estado ENUM('disponible', 'prestado', 'en_mora', 'perdido') NULL DEFAULT 'disponible',
-  PRIMARY KEY (ejemplar_id),
-  INDEX libro_id (ejemplar_libro_id ASC) ,
-  INDEX idx_ejemplar_estado (ejemplar_estado ASC) ,
-  CONSTRAINT ejemplar_ibfk_1
-    FOREIGN KEY (ejemplar_libro_id)
-    REFERENCES biblioteca.libro (libro_id)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+DROP TABLE IF EXISTS `prestamo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prestamo` (
+  `prestamo_id` int(11) NOT NULL AUTO_INCREMENT,
+  `prestamo_prestatario_id` int(11) NOT NULL,
+  `prestamo_bibliotecario_id` int(11) NOT NULL,
+  `prestamo_fecha_prestamo` datetime DEFAULT current_timestamp(),
+  `prestamo_fecha_devolucion_prevista` datetime NOT NULL,
+  `prestamo_fecha_devolucion_real` datetime DEFAULT NULL,
+  PRIMARY KEY (`prestamo_id`),
+  KEY `prestatario_id` (`prestamo_prestatario_id`),
+  KEY `bibliotecario_id` (`prestamo_bibliotecario_id`),
+  CONSTRAINT `prestamo_ibfk_1` FOREIGN KEY (`prestamo_prestatario_id`) REFERENCES `prestatario` (`prestatario_id`),
+  CONSTRAINT `prestamo_ibfk_2` FOREIGN KEY (`prestamo_bibliotecario_id`) REFERENCES `usuario` (`usuario_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `prestamoejemplar`
+--
 
--- -----------------------------------------------------
--- Table biblioteca.libroautor_libroautor
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.libroautor_libroautor ;
+DROP TABLE IF EXISTS `prestamoejemplar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prestamoejemplar` (
+  `prestamoejemplar_id` int(11) NOT NULL AUTO_INCREMENT,
+  `prestamoejemplar_prestamo_id` int(11) NOT NULL,
+  `prestamoejemplar_ejemplar_id` int(11) NOT NULL,
+  PRIMARY KEY (`prestamoejemplar_id`),
+  KEY `ejemplar_id` (`prestamoejemplar_ejemplar_id`),
+  KEY `prestamoejemplar_ibfk_1` (`prestamoejemplar_prestamo_id`),
+  CONSTRAINT `prestamoejemplar_ibfk_1` FOREIGN KEY (`prestamoejemplar_prestamo_id`) REFERENCES `prestamo` (`prestamo_id`) ON DELETE CASCADE,
+  CONSTRAINT `prestamoejemplar_ibfk_2` FOREIGN KEY (`prestamoejemplar_ejemplar_id`) REFERENCES `ejemplar` (`ejemplar_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS biblioteca.libroautor_libroautor (
-  libroautor_id INT NOT NULL  AUTO_INCREMENT,
-  libroautor_libro_id INT(11) NOT NULL,
-  autor_id INT(11) NOT NULL,
-  INDEX autor_id (autor_id ASC) ,
-  PRIMARY KEY (libroautor_id),
-  CONSTRAINT libroautor_ibfk_1
-    FOREIGN KEY (libroautor_libro_id)
-    REFERENCES biblioteca.libro (libro_id)
-    ON DELETE CASCADE,
-  CONSTRAINT libroautor_ibfk_2
-    FOREIGN KEY (autor_id)
-    REFERENCES biblioteca.autor (autor_id)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+--
+-- Table structure for table `prestatario`
+--
 
+DROP TABLE IF EXISTS `prestatario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prestatario` (
+  `prestatario_id` int(11) NOT NULL AUTO_INCREMENT,
+  `prestatario_nombre` varchar(100) NOT NULL,
+  `prestatario_identificacion` varchar(20) NOT NULL,
+  `prestatario_fecha_creacion` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`prestatario_id`),
+  UNIQUE KEY `identificacion` (`prestatario_identificacion`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table biblioteca.prestatario
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.prestatario ;
+--
+-- Table structure for table `rol`
+--
 
-CREATE TABLE IF NOT EXISTS biblioteca.prestatario (
-  prestatario_id INT(11) NOT NULL AUTO_INCREMENT,
-  prestatario_nombre VARCHAR(100) NOT NULL,
-  prestatario_identificacion VARCHAR(20) NOT NULL,
-  prestatario_fecha_creacion DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (prestatario_id),
-  UNIQUE INDEX identificacion (prestatario_identificacion ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+DROP TABLE IF EXISTS `rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rol` (
+  `rol_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rol_nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`rol_id`),
+  UNIQUE KEY `nombre` (`rol_nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `usuario`
+--
 
--- -----------------------------------------------------
--- Table biblioteca.rol
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.rol ;
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `usuario_id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario_nombre` varchar(100) NOT NULL,
+  `usuario_email` varchar(100) NOT NULL,
+  `usuario_contrasena` varchar(255) NOT NULL,
+  `usuario_rol_id` int(11) NOT NULL,
+  `usuario_fecha_registro` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`usuario_id`),
+  UNIQUE KEY `email` (`usuario_email`),
+  KEY `rol_id` (`usuario_rol_id`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`usuario_rol_id`) REFERENCES `rol` (`rol_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-CREATE TABLE IF NOT EXISTS biblioteca.rol (
-  rol_id INT(11) NOT NULL AUTO_INCREMENT,
-  rol_nombre VARCHAR(50) NOT NULL,
-  PRIMARY KEY (rol_id),
-  UNIQUE INDEX nombre (rol_nombre ASC) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
--- -----------------------------------------------------
--- Table biblioteca.usuario
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.usuario ;
-
-CREATE TABLE IF NOT EXISTS biblioteca.usuario (
-  usuario_id INT(11) NOT NULL AUTO_INCREMENT,
-  usuario_nombre VARCHAR(100) NOT NULL,
-  usuario_email VARCHAR(100) NOT NULL,
-  usuario_contrasena VARCHAR(255) NOT NULL,
-  usuario_rol_id INT(11) NOT NULL,
-  usuario_fecha_registro DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (usuario_id),
-  UNIQUE INDEX email (usuario_email ASC) ,
-  INDEX rol_id (usuario_rol_id ASC) ,
-  CONSTRAINT usuario_ibfk_1
-    FOREIGN KEY (usuario_rol_id)
-    REFERENCES biblioteca.rol (rol_id)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table biblioteca.prestamo
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.prestamo ;
-
-CREATE TABLE IF NOT EXISTS biblioteca.prestamo (
-  prestamo_id INT(11) NOT NULL AUTO_INCREMENT,
-  prestamo_prestatario_id INT(11) NOT NULL,
-  prestamo_bibliotecario_id INT(11) NOT NULL,
-  prestamo_fecha_prestamo DATETIME NULL DEFAULT CURRENT_TIMESTAMP(),
-  prestamo_fecha_devolucion_prevista DATETIME NOT NULL,
-  prestamo_fecha_devolucion_real DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (prestamo_id),
-  INDEX prestatario_id (prestamo_prestatario_id ASC) ,
-  INDEX bibliotecario_id (prestamo_bibliotecario_id ASC) ,
-  CONSTRAINT prestamo_ibfk_1
-    FOREIGN KEY (prestamo_prestatario_id)
-    REFERENCES biblioteca.prestatario (prestatario_id),
-  CONSTRAINT prestamo_ibfk_2
-    FOREIGN KEY (prestamo_bibliotecario_id)
-    REFERENCES biblioteca.usuario (usuario_id))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table biblioteca.prestamoejemplar
--- -----------------------------------------------------
-DROP TABLE IF EXISTS biblioteca.prestamoejemplar ;
-
-CREATE TABLE IF NOT EXISTS biblioteca.prestamoejemplar (
-  prestamoejemplar_id INT NOT NULL  AUTO_INCREMENT,
-  prestamoejemplar_prestamo_id INT(11) NOT NULL,
-  prestamoejemplar_ejemplar_id INT(11) NOT NULL,
-  INDEX ejemplar_id (prestamoejemplar_ejemplar_id ASC) ,
-  PRIMARY KEY (prestamoejemplar_id),
-  CONSTRAINT prestamoejemplar_ibfk_1
-    FOREIGN KEY (prestamoejemplar_prestamo_id)
-    REFERENCES biblioteca.prestamo (prestamo_id)
-    ON DELETE CASCADE,
-  CONSTRAINT prestamoejemplar_ibfk_2
-    FOREIGN KEY (prestamoejemplar_ejemplar_id)
-    REFERENCES biblioteca.ejemplar (ejemplar_id)
-    ON DELETE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+-- Dump completed on 2025-05-20 23:39:48
